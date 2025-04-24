a73671388f9adccd5a528d801d6ee4941be8cf88
eyobScript <eyobbmulugeta@gmail.com>
2025-04-24 23:17:37 +0300

    chack the stack is empty

--- search/maze.py
+++ search/maze.py
@@ -14,3 +14,9 @@ class StackFrontier():
 
     def add(self,node):
         self.frontier.append(node)
+
+    def contains_state(self, state):
+        return any(node.state == state for node in self.frontier)
+
+    def empty(self):
+        return len(self.frontier) == 0
