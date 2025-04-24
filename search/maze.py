c138d83adad3a86aff392ec744d71b8b3ed162c1
eyobScript <eyobbmulugeta@gmail.com>
2025-04-24 23:29:16 +0300

    remove

--- search/maze.py
+++ search/maze.py
@@ -20,3 +20,11 @@ class StackFrontier():
 
     def empty(self):
         return len(self.frontier) == 0
+
+    def remove(self):
+        if self.empty():
+            raise Exception('frontier is empty')
+        else:
+            node = self.frontier[-1]
+            self.frontier = self.frontier[:-1]
+            return node
